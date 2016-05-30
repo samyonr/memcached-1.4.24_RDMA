a9926d388bb3654ea76020b99ad06b0d5041386b
samyonr <samyon.ristov@mail.huji.ac.il>
2016-05-30 14:50:54 +0300

    including libxio

---
+++ memcached-1.4.24_RDMA/backup_rdma_accelio.c
@@ -0,0 +1,226 @@
+/*
+ * The code below is a modification of Mellanox's server and client examples.
+ * Keeping their disclaimer.
+ */
+
+/*
+ * Copyright (c) 2013 Mellanox Technologies®. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two licenses.
+ * You may choose to be licensed under the terms of the GNU General Public
+ * License (GPL) Version 2, available from the file COPYING in the main
+ * directory of this source tree, or the Mellanox Technologies® BSD license
+ * below:
+ *
+ *      - Redistribution and use in source and binary forms, with or without
+ *        modification, are permitted provided that the following conditions
+ *        are met:
+ *
+ *      - Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *      - Neither the name of the Mellanox Technologies® nor the names of its
+ *        contributors may be used to endorse or promote products derived from
+ *        this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <errno.h>
+
+#include "libxio.h"
+
+#define QUEUE_DEPTH		512
+#define PRINT_COUNTER		4000000
+#define DISCONNECT_NR		(2 * PRINT_COUNTER)
+
+int test_disconnect;
+
+/* server private data */
+struct server_data {
+	struct xio_context	*ctx;
+	struct xio_connection	*connection;
+	uint64_t		nsent;
+	uint64_t		cnt;
+	int			pad;
+	int			ring_cnt;
+	struct xio_msg		rsp_ring[QUEUE_DEPTH];	/* global message */
+};
+
+/*---------------------------------------------------------------------------*/
+/* ring_get_next_msg							     */
+/*---------------------------------------------------------------------------*/
+static inline struct xio_msg *ring_get_next_msg(struct server_data *sd)
+{
+	struct xio_msg	*msg = &sd->rsp_ring[sd->ring_cnt++];
+
+	if (sd->ring_cnt == QUEUE_DEPTH)
+		sd->ring_cnt = 0;
+
+	return msg;
+}
+
+/*---------------------------------------------------------------------------*/
+/* process_request							     */
+/*---------------------------------------------------------------------------*/
+static void process_request(struct server_data *server_data,
+			    struct xio_msg *req)
+{
+	struct xio_iovec_ex	*sglist = vmsg_sglist(&req->in);
+	char			*str;
+	int			nents = vmsg_sglist_nents(&req->in);
+	int			len, i;
+	char			tmp;
+
+	/* note all data is packed together so in order to print each
+	 * part on its own NULL character is temporarily stuffed
+	 * before the print and the original character is restored after
+	 * the printf
+	 */
+	if (++server_data->cnt == PRINT_COUNTER) {
+		str = (char *)req->in.header.iov_base;
+		len = req->in.header.iov_len;
+		if (str) {
+			if (((unsigned)len) > 64)
+				len = 64;
+			tmp = str[len];
+			str[len] = '\0';
+			printf("message header : [%llu] - %s\n",
+			       (unsigned long long)(req->sn + 1), str);
+			str[len] = tmp;
+		}
+		for (i = 0; i < nents; i++) {
+			str = (char *)sglist[i].iov_base;
+			len = sglist[i].iov_len;
+			if (str) {
+				if (((unsigned)len) > 64)
+					len = 64;
+				tmp = str[len];
+				str[len] = '\0';
+				printf("message data: [%llu][%d][%d] - %s\n",
+				       (unsigned long long)(req->sn + 1),
+				       i, len, str);
+				str[len] = tmp;
+			}
+		}
+		server_data->cnt = 0;
+	}
+	req->in.header.iov_base	  = NULL;
+	req->in.header.iov_len	  = 0;
+	vmsg_sglist_set_nents(&req->in, 0);
+}
+
+/*---------------------------------------------------------------------------*/
+/* on_session_event							     */
+/*---------------------------------------------------------------------------*/
+static int on_session_event(struct xio_session *session,
+			    struct xio_session_event_data *event_data,
+			    void *cb_user_context)
+{
+	struct server_data *server_data = (struct server_data *)cb_user_context;
+
+	printf("session event: %s. session:%p, connection:%p, reason: %s\n",
+	       xio_session_event_str(event_data->event),
+	       (void *)session, (void *)event_data->conn,
+	       xio_strerror(event_data->reason));
+
+	switch (event_data->event) {
+	case XIO_SESSION_NEW_CONNECTION_EVENT:
+		server_data->connection = event_data->conn;
+		break;
+	case XIO_SESSION_CONNECTION_TEARDOWN_EVENT:
+		xio_connection_destroy(event_data->conn);
+		server_data->connection = NULL;
+		break;
+	case XIO_SESSION_TEARDOWN_EVENT:
+		xio_session_destroy(session);
+		xio_context_stop_loop(server_data->ctx);  /* exit */
+		break;
+	default:
+		break;
+	};
+
+	return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+/* on_new_session							     */
+/*---------------------------------------------------------------------------*/
+static int on_new_session(struct xio_session *session,
+			  struct xio_new_session_req *req,
+			  void *cb_user_context)
+{
+	struct server_data *server_data = (struct server_data *)cb_user_context;
+
+	/* automatically accept the request */
+	printf("new session event. session:%p\n", (void*)session);
+
+	if (!server_data->connection)
+		xio_accept(session, NULL, 0, NULL, 0);
+	else
+		xio_reject(session, (enum xio_status)EISCONN, NULL, 0);
+
+	return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+/* on_request callback							     */
+/*---------------------------------------------------------------------------*/
+static int on_request(struct xio_session *session,
+		      struct xio_msg *req,
+		      int last_in_rxq,
+		      void *cb_user_context)
+{
+	struct server_data *server_data = (struct server_data *)cb_user_context;
+	struct xio_msg	   *rsp = ring_get_next_msg(server_data);
+
+	/* process request */
+	process_request(server_data, req);
+
+	/* attach request to response */
+	rsp->request = req;
+
+	xio_send_response(rsp);
+	server_data->nsent++;
+
+	if (test_disconnect) {
+		if (server_data->nsent == DISCONNECT_NR) {
+			xio_disconnect(server_data->connection);
+			return 0;
+		}
+	}
+	return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+/* asynchronous callbacks						     */
+/*---------------------------------------------------------------------------*/
+static struct xio_session_ops  server_ops __attribute__ ((unused)) = {
+	.on_session_event		=  on_session_event,
+	.on_new_session			=  on_new_session,
+	.on_msg_send_complete		=  NULL,
+	.on_msg				=  on_request,
+	.on_msg_error			=  NULL
+};
+
+/*---------------------------------------------------------------------------*/
+/* main									     */
+/*---------------------------------------------------------------------------*/
+
---
+++ memcached-1.4.24_RDMA/backup_rdma_accelio.h
@@ -0,0 +1,15 @@
+/*
+ * backup_rdma_accelio.h
+ *
+ *  Created on: Apr 4, 2016
+ *      Author: a315
+ */
+
+#ifndef BACKUP_RDMA_ACCELIO_H_
+#define BACKUP_RDMA_ACCELIO_H_
+
+
+
+
+
+#endif /* BACKUP_RDMA_ACCELIO_H_ */
